a9fe25734d9b53b524c8997840ae89bc564487e7
amine omari <omari1amineco@gmail.com>
2025-01-21 14:38:01 +0300

    feat: add square

--- components/Task.js
+++ components/Task.js
@@ -1,14 +1,28 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 const Task = ({ text }) => {
   return (
-    <View>
-      <Text>{text}</Text>
+    <View style={styles.item}>
+      <View style={styles.itemLeft}>
+        <TouchableOpacity style={styles.square}></TouchableOpacity>
+
+        <Text style={styles.text}>{text}</Text>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  item: {
+    backgroundColor: "#fff",
+    padding: 15,
+    borderRadius: 10,
+  },
+  itemLeft: {},
+  square: {},
+  text: {},
+  circular: {},
+});
 
 export default Task;
