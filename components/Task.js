5e56ae77e6ca2439994fc15e5e782e362586db55
amine omari <omari1amineco@gmail.com>
2025-01-21 14:26:39 +0300

    feat: add text

--- components/Task.js
+++ components/Task.js
@@ -2,7 +2,13 @@ import React from "react";
 import { Text, View, StyleSheet } from "react-native";
 
 const Task = () => {
-  return <View></View>;
+  return (
+    <View>
+      <Text>this is a Task</Text>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({});
 
 export default Task;
